65507797a8c648cd094d507ba38f10c8479f5a8a
medha37 <63610669+medha37@users.noreply.github.com>
2020-07-08 13:35:46 +0530

    3rd step design + coding
    set animation and correct incorrect class in option and their color changing property

--- app.js
+++ app.js
@@ -34,16 +34,58 @@ function getNewQuestion(){
     for(let i=0; i<optionLen; i++){
         availableOptions.push(i)
     }  
+
+    let animationDelay = 0.15;
     //create options in html
+    optionContainer.innerHTML=" ";
     for(let i=0; i<optionLen; i++){
+        //random option
+        const optionIndex = availableOptions[Math.floor(Math.random()*availableQuestions.length)];
+        //get the position of optionindex from the available questions
+        const index2 = availableOptions.indexOf(optionIndex);
+        //get position of of optionIndex from the availableOptions,so that the option does not repeat
+        availableOptions.splice(index2,1);
         const option = document.createElement("div");
-        option.innerHTML = currentQuestion.options[i];
-        option.id = i;
+        option.innerHTML = currentQuestion.options[optionIndex];
+        option.id = optionIndex;
+        option.style.animationDelay = animationDelay + 's';
+        animationDelay = animationDelay + 0.15;
         option.className= "option";
         optionContainer.appendChild(option)
+        option.setAttribute("onclick" ,"getResult(this)");
     }
     questionCounter++
 }
+
+//get the result of current attempt question
+function getResult(element){
+    const id = parseInt(element.id);
+    //geet the answer by comparing the id of clicked option 
+    if(id === currentQuestion.answer){
+        //set the green clr to the correct option 
+        element.classList.add("correct");
+    }
+    else{
+        //set the red clr to the incorrect option 
+        element.classList.add("wrong");
+        //if the answer is incorrect then show the correct ans by adding green clr 
+        const optionLen = optionContainer.children.length;
+        for(let i=0; i<optionLen; i++){
+            if(parseInt( optionContainer.children[i].id) === currentQuestion.answer){
+                optionContainer.children[i].classList.add("correct");
+            }
+        }
+    }
+    unclickableOptions();
+
+}
+//make all the options unclickable once the user select any option 
+function unclickableOptions(){
+    const optionLen = optionContainer.children.length;
+    for(let i=0; i<optionLen; i++){
+        optionContainer.children[i].classList.add("already-clicked");
+    }
+};
 
 function next(){
     if(questionCounter === quiz.length){
