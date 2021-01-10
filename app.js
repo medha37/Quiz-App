9953685dfb2a067f915e779a160ad63e5a57e75f
medha37 <63610669+medha37@users.noreply.github.com>
2021-01-10 18:37:20 +0530

    Add question list
    1.render question list
    2.render question form

--- app.js
+++ app.js
@@ -9,23 +9,74 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 
 // database connection
-mongoose.connect("mongodb+srv://dbuser:dbpassword@cluster0.l4plx.mongodb.net/test?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true 
-    })
-    .then(function(){console.log("MongoDB Connected...")})
-    .catch(function(err){console.log(err)});
+// mongoose.connect("mongodb+srv://dbuser:dbpassword@cluster0.l4plx.mongodb.net/test?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true 
+//     })
+//     .then(function(){console.log("MongoDB Connected...")})
+//     .catch(function(err){console.log(err)});
 
-// Schema definition
-let questionSchema = new mongoose.Schema({
-  q: String,
-  options: [String],
-  answer: Number
-});
-// Compile into model
-let Question = mongoose.model("Question", questionSchema);
-
+// // Schema definition
+// let questionSchema = new mongoose.Schema({
+//   q: String,
+//   options: [String],
+//   answer: Number
+// });
+// // Compile into model
+// let Question = mongoose.model("Question", questionSchema);
+const quiz = [
+  {
+      q:'Which is the part of the computer system that one can physically touch?',
+      options: ['data' , 'operating systems ' , 'hardware' , 'software'],
+      answer: 2
+  },
+  {
+      q:'A ………. is an electronic device that process data, converting it into information.',
+      options:['computer ' , 'processor' , 'case' , 'stylus'],
+      answer: 0
+  },
+  {
+      q:'IC chips used in computers are usually made of:',
+      options: ['Lead ' , 'Silicon' , 'Chromium' , 'Gold'],
+      answer: 1
+  },
+  {
+      q:'Which of the following is not an example of an Operating System?',
+      options: ['Windows 98 ' , 'BSD Unix' , 'Microsoft Office XP' , 'Red Hat Linux'],
+      answer: 2
+  },
+  {
+      q:'Which of the following is not an input device?',
+      options: ['Mouse ' , 'Light pen' , 'Keyboard' , ' VDU'],
+      answer: 3
+  },
+  {
+      q:'One Gigabyte is approximately equal is:',
+      options: ['1000,000 bytes' , '1000,000,000 bytes' , '1000,000,000,000 bytes' , ' None of these'],
+      answer: 1
+  },
+  {
+      q:' Compact discs, (according to the original CD specifications) hold how many minutes of music?',
+      options: [' 74 mins' , '90 mins' , '56 mins' , '60 mins'],
+      answer: 0
+  },
+  {
+      q:' What type of process creates a smaller file that is faster to transfer over the internet?',
+      options: ['Compression' , 'Fragmentation' , 'Encapsulation' , ' None of the above'],
+      answer: 0
+  },
+  {
+      q:'Which of the following is used to Manage DataBase?',
+      options: ['Operating System' , 'Compiler' , 'DBMS' , ' None of the above'],
+      answer: 2
+  }, 
+  {
+      q:' Which of the following is an example of non-volatile memory?',
+      options: ['Cache memory' , ' RAM' , 'ROM' , ' None of the above'],
+      answer: 2
+  }                                
+]
 
 
 //setting routes
@@ -37,12 +88,12 @@ app.get("/",function(req,res){
 
 //List of questions
 app.get("/questions",function(req,res){
-
+  res.render("questions",{questions:quiz});
 });
 
 //Add question form
 app.get("/questions/new",function(req,res){
-
+    res.render("form");
 });
 
 //Add question to database
