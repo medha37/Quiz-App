03213ad388ee5067612f8df086906f46c81e4231
Ravi Keshri <61163354+ravikeshri@users.noreply.github.com>
2021-01-10 17:20:41 +0530

    add MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 //requiring packages
-const express = require("express"),
-app = express();
+const express   = require("express"),
+      app       = express(),
+      mongoose  = require("mongoose");
+
 
 //setup
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+// database connection
+mongoose.connect("mongodb+srv://dbuser:dbpassword@cluster0.l4plx.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    })
+    .then(function(){console.log("MongoDB Connected...")})
+    .catch(function(err){console.log(err)});
+
+// Schema definition
+let questionSchema = new mongoose.Schema({
+  q: String,
+  options: [String],
+  answer: Number
+});
+// Compile into model
+let Question = mongoose.model("Question", questionSchema);
+
+
+
 //setting routes
 
 //dashboard
-
 app.get("/",function(req,res){
     res.render("quiz");
 });
