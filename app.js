62b2a254ff0c006aa486f83b1bf1377da162f4c0
medha37 <63610669+medha37@users.noreply.github.com>
2021-01-16 20:51:48 +0530

    adding & deleting ques in db
    ....

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express   = require("express"),
       app       = express(),
       bodyParser = require("body-parser"),
+     methodOverride = require('method-override'),
       mongoose  = require("mongoose");
 
 
@@ -9,76 +10,26 @@ const express   = require("express"),
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // database connection
-// mongoose.connect("mongodb+srv://dbuser:dbpassword@cluster0.l4plx.mongodb.net/test?retryWrites=true&w=majority",
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true 
-//     })
-//     .then(function(){console.log("MongoDB Connected...")})
-//     .catch(function(err){console.log(err)});
+ mongoose.connect("mongodb+srv://dbuser:dbpassword@cluster0.l4plx.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(function(){console.log("MongoDB Connected...")})
+    .catch(function(err){console.log(err)});
 
-// // Schema definition
-// let questionSchema = new mongoose.Schema({
-//   q: String,
-//   options: [String],
-//   answer: Number
-// });
-// // Compile into model
-// let Question = mongoose.model("Question", questionSchema);
-let quiz = [
-  {
-      q:'Which is the part of the computer system that one can physically touch?',
-      options: ['data' , 'operating systems ' , 'hardware' , 'software'],
-      answer: 2
-  },
-  {
-      q:'A ………. is an electronic device that process data, converting it into information.',
-      options:['computer ' , 'processor' , 'case' , 'stylus'],
-      answer: 0
-  },
-  {
-      q:'IC chips used in computers are usually made of:',
-      options: ['Lead ' , 'Silicon' , 'Chromium' , 'Gold'],
-      answer: 1
-  },
-  {
-      q:'Which of the following is not an example of an Operating System?',
-      options: ['Windows 98 ' , 'BSD Unix' , 'Microsoft Office XP' , 'Red Hat Linux'],
-      answer: 2
-  },
-  {
-      q:'Which of the following is not an input device?',
-      options: ['Mouse ' , 'Light pen' , 'Keyboard' , ' VDU'],
-      answer: 3
-  },
-  {
-      q:'One Gigabyte is approximately equal is:',
-      options: ['1000,000 bytes' , '1000,000,000 bytes' , '1000,000,000,000 bytes' , ' None of these'],
-      answer: 1
-  },
-  {
-      q:' Compact discs, (according to the original CD specifications) hold how many minutes of music?',
-      options: [' 74 mins' , '90 mins' , '56 mins' , '60 mins'],
-      answer: 0
-  },
-  {
-      q:' What type of process creates a smaller file that is faster to transfer over the internet?',
-      options: ['Compression' , 'Fragmentation' , 'Encapsulation' , ' None of the above'],
-      answer: 0
-  },
-  {
-      q:'Which of the following is used to Manage DataBase?',
-      options: ['Operating System' , 'Compiler' , 'DBMS' , ' None of the above'],
-      answer: 2
-  }, 
-  {
-      q:' Which of the following is an example of non-volatile memory?',
-      options: ['Cache memory' , ' RAM' , 'ROM' , ' None of the above'],
-      answer: 2
-  }                                
-]
+// Schema definition
+let questionSchema = new mongoose.Schema({
+  q: String,
+  options: [String],
+  answer: Number
+});
+// Compile into model
+let Question = mongoose.model("Question", questionSchema);
 
 
 //setting routes
@@ -93,7 +44,15 @@ app.get("/questions",function(req,res){
     // FIND all questions then 
     // render questions list
 
-    res.render("questions", { questions:quiz });
+    Question.find({},function(err,questions){
+        if(err)
+          console.log(err);
+        else{
+            res.render("questions", { questions:questions});
+        }
+    });
+
+    
 });
 
 //Add question form
@@ -110,13 +69,21 @@ app.post("/questions",function(req,res){
     answer: Number(req.body.ans)-1
 };
 
-  quiz.push(newQ);
+
 
   // CREATE question
   // if error redirect form again
   // else redirect to questions list
 
-  res.redirect("/questions");
+  Question.create(newQ,function(err,createdQ){
+      if(err)
+         res.redirect("/questions/new");
+        else{
+            res.redirect("/questions");
+        }
+  });
+
+  
 });
 
 app.get("/questions/:qid/edit", function(req, res) {
@@ -136,6 +103,13 @@ app.put("/questions/:qid", function(req, res) {
 app.delete("/questions/:qid",function(req,res){
     // findByIdAndRemove the question
     // after that redirect to qustions list
+    Question.findByIdAndRemove(req.params.qid,function(err,removedQ){
+        if(err)
+         res.redirect("/questions");
+         else{
+             res.redirect("/questions");
+         }
+    });
     
 });
 
